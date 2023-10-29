a54ffe05be740501407c63c70c38c4f2aa2c1757
Marija Zlatkova <marijazlatkova44@yahoo.com>
2023-10-30 00:34:46 +0100

    enhanced database connection

--- OfficeApp/pkg/db/index.js
+++ OfficeApp/pkg/db/index.js
@@ -3,15 +3,12 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: `${__dirname}/../../config.env` });
 
-const db = process.env.DATABASE
-  .replace("<USERNAME>", process.env.DATABASE_USERNAME)
-  .replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
-
-(async function () {
+(async function() {
   try {
+    const db = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.tonpgxf.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`;
     await mongoose.connect(db);
-    console.log("Successfully connected to DataBase");
+    console.log("Successfully connected to database");
   } catch (err) {
-    console.log("Error connecting to DataBase");
+    console.error("Error connecting to database", err);
   }
 })();
